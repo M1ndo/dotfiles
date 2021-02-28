9cfabbd77201cab4b413fb5d04bdc97741c33ce5
ybenel <ybenel@molero.xyz>
2021-02-28 12:37:33 +0100

    Updating The Conf

--- .config/herbstluftwm/xmenu.sh
+++ .config/herbstluftwm/xmenu.sh
@@ -3,14 +3,17 @@
 cat <<EOF | xmenu | sh &
 IMG:./icons/browser.png	Browser	google-chrome-stable
 IMG:./icons/stremio.png	Stremio	stremio
+IMG:./icons/discord.png	Discord	Discord
 IMG:./icons/atom.png	Atom	atom
 IMG:./icons/gimp.png	Gimp	gimp
+IMG:./icons/spotify.png Spotify spot_load
+IMG:./icons/pcmanfm.png PCManFM pcmanfm
 IMG:./icons/telegram.png	Telegram	telegram-desktop
-IMG:./icons/discord.png	Discord	discord
 IMG:./icons/xterm.png	Terminal	xterm
 
 IMG:./icons/sleep.png	Sleep	xscreensaver-command -lock
 IMG:./icons/hibernate.png	Hibernate	systemctl -i hibernate
+IMG:./icons/suspend.png Suspend systemctl -i suspend
 IMG:./icons/restart.png	Restart	systemctl -i reboot
 IMG:./icons/shutdown.png	Shutdown	shutdown -P now	
 EOF
