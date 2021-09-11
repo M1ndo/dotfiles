b9d06d3c21aa1a07b42d5b78c0c43a6dc80280fd
ybenel <ybenel@molero.xyz>
2021-09-11 13:26:49 +0100

    Adding Polybar Rounded

---
+++ .config/herbstluftwm/start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function run { 
+    CMD=$(echo $1 | awk '{print $1}')
+    if [[ -z $(pgrep $CMD) ]]; then
+        echo "This doesn't have sxhkd";
+        $1 &
+    fi
+}
+run "picom"
+run "nm-applet"
+run "nitrogen --restore"
+run "numlockx on"
+run "xfce4-power-manager"
+run "/usr/lib/polkit-kde-authentication-agent-1"
+run "/usr/bin/emacs --daemon"
+run "xscreensaver -no-splash"
+run "caffeine"
+run "unclutter -root"
+run "volumeicon"
+run "sxhkd"
+run "mpd"
